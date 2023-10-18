60859fd3e93eceb82406731c484313ca52e9bdc2
Gustavo <gustavo.beleze@gmail.com>
2023-10-18 05:53:54 -0300

    fim capitulo estado

--- src/pages/estado/calculadora.tsx
+++ src/pages/estado/calculadora.tsx
@@ -2,48 +2,23 @@ import PagComponente from "@/components/pagina";
 import { useState } from "react";
 
 export default function Calculadora() {
-  const [num1, setNum1] = useState("0");
-  const [num2, setNum2] = useState("0");
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
 
-  function alterarNum1(e: any) {
-    const resultado = document.getElementById("result");
-    resultado.className = "hidden";
-    setNum1(e.target.value);
+  function soma(n1: number, n2: number) {
+    return n1 + n2;
   }
 
-  function alterarNum2(e: any) {
-    const resultado = document.getElementById("result");
-    resultado.className = "hidden";
-    setNum2(e.target.value);
+  function multiplicacao(n1: number, n2: number) {
+    return n1 * n2;
   }
 
-  function soma(n1: string, n2: string) {
-    const a = parseInt(n1);
-    const b = parseInt(n2);
-    return a + b;
+  function divisao(n1: number, n2: number) {
+    return n1 / n2;
   }
 
-  function multiplicacao(n1: string, n2: string) {
-    const a = parseInt(n1);
-    const b = parseInt(n2);
-    return a * b;
-  }
-
-  function divisao(n1: string, n2: string) {
-    const a = parseInt(n1);
-    const b = parseInt(n2);
-    return a / b;
-  }
-
-  function subtracao(n1: string, n2: string) {
-    const a = parseInt(n1);
-    const b = parseInt(n2);
-    return a - b;
-  }
-
-  function show() {
-    const resultado = document.getElementById("result");
-    resultado.className = "flex flex-col gap-8 items-center";
+  function subtracao(n1: number, n2: number) {
+    return n1 - n2;
   }
 
   return (
@@ -58,21 +33,21 @@ export default function Calculadora() {
           <input
             type="number"
             className="campo w-64"
-            onChange={alterarNum1}
+            onChange={e => setNum1(+e.target.value)}
             value={num1}
           />
           <input
             type="number"
             className="campo w-64"
-            onChange={alterarNum2}
+            onChange={e => setNum2(+e.target.value)}
             value={num2}
           />
         </div>
         <div className="flex flex-col gap-6 items-center">
-          <button className="botao" onClick={show}>
+          {/* <button className="botao" onClick={show}>
             Calcular
-          </button>
-          <div className="hidden" id="result">
+          </button> */}
+          <div className="flex flex-col gap-8 items-center" id="result">
             <span>
               {num1} + {num2} = {soma(num1, num2)}
             </span>
